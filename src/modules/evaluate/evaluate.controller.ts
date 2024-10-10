d65eb80888ab2f6d3075344e899b6290231a1595
Oleg <test@test.com>
2024-10-10 13:21:18 +0100

    feat: add error handling for evaluate method

--- src/modules/evaluate/evaluate.controller.ts
+++ src/modules/evaluate/evaluate.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from "@nestjs/common";
 import { EvaluateService } from "./evaluate.service";
 import { TypedBody, TypedRoute } from "@nestia/core";
-import { EvaluateDto } from "./evaluate.dto";
+import { EvaluateDto, EvaluateResponse } from "./evaluate.dto";
 import { ApiTags } from "@nestjs/swagger";
 
 @ApiTags("evaluate")
@@ -10,7 +10,15 @@ export class EvaluateController {
   constructor(private readonly evaluateService: EvaluateService) {}
 
   @TypedRoute.Post("/")
-  public evaluate(@TypedBody() body: EvaluateDto) {
-    return this.evaluateService.evaluate(body.expression);
+  public evaluate(@TypedBody() body: EvaluateDto): EvaluateResponse {
+    try {
+      const result = this.evaluateService.evaluate(body.expression);
+
+      return { result };
+    } catch (err) {
+      // Todo implement logger
+      console.log(err);
+      return { errorMessage: err.message };
+    }
   }
 }
